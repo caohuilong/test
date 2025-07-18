938d69804267e006e78fb8f1b2e59911c2a765ff
caohuilong <caohuilong@corp.netease.com>
2025-07-18 16:48:23 +0800

    update v2

--- script.js
+++ script.js
@@ -18,3 +18,12 @@ btn.onclick = function() {
     idx = (idx + 1) % loveWords.length;
     p.innerText = loveWords[idx];
 };
+
+// 网易云音乐 iframe 自动播放提示
+window.onload = function() {
+    // 有些浏览器会阻止自动播放，需要用户首次点击页面
+    // 可以弹窗提示用户点击播放
+    setTimeout(function(){
+        alert('如果没有自动播放音乐，请点击页面任意位置或播放器播放按钮哦~');
+    }, 1500);
+};
